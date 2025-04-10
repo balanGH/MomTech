e6d1e568a18969140ead2ea0fba53d9c269bf79b
balanGH <152583609+balanGH@users.noreply.github.com>
2025-04-10 10:17:56 +0530

    Update index.tsx

--- frontend/app/(tabs)/mom/index.tsx
+++ frontend/app/(tabs)/mom/index.tsx
@@ -4,7 +4,6 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {baseURL} from '@/app/api/api';
 
 export default function HomeScreen() {
   const router = useRouter();
